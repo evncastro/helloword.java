0bff950fabdb9dbee68a2192a22afe95dda1761b
evncastro <viniciusgoncalves57003@gmail.com>
2024-08-22 00:54:42 +0000

    aula balançotrimestral

---
+++ TesteIdade.java
@@ -0,0 +1,15 @@
+class testeIdade {
+    public static void main(string[] args) {
+        int idade ;
+        idade = 20;
+        // system.out.println(idade);
+      int  idadeanoquevem = idade  + 1;
+      system.out.println(idadanoquevem);
+      double pi = 3.14;
+      int raio = 5;
+      double result = 2 * raio * pi;
+      system.out.println (result);
+      boolean menordeidade = idade < 18;
+      systen.out.println(menordeidade);
+    }
+}
